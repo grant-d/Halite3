2eb8e782916d3a30b37622baaaebd833745834a0
Grant Dickinson <grant@k2.com>
2018-12-12 23:25:37 -0800

    Bot-v06

--- Halite3/hlt/CostField.cs
+++ Halite3/hlt/CostField.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 
 namespace Halite3.Hlt
@@ -35,6 +36,17 @@ namespace Halite3.Hlt
 
             _cells = new CostCell[Height * Width];
 
+            // If mine <= 9, then moveCost = 9/10 == 0. So we must always keep mine >= 10.
+            // Then add enough for one more dig: 10 * 4/3 = 13.33. 13.33 * 0.75 == 10.
+            double ratio = (Constants.ExtractRatio - 1.0) / Constants.ExtractRatio; // 0.75
+            double extra = Constants.MoveCostRatio / ratio; // 13.33
+            double log75 = Math.Log(ratio);
+
+            // halite * 0.75^p == 13.33
+            double Potential(double halite) => Math.Max(0, Math.Log(extra / halite) / log75);
+
+            double maxPotential = Potential(maxHalite);
+
             for (int y = 0; y < Height; y++)
             {
                 for (int x = 0; x < Width; x++)
@@ -55,15 +67,18 @@ namespace Halite3.Hlt
                     }
                     else
                     {
-                        const int factor = byte.MaxValue - 2; // 253
+                        double halite = Math.Max(1, mapCell.Halite);
 
                         // Normalize the amount of halite
-                        int halite = 1 + mapCell.Halite * factor / maxHalite; // 1..254
+                        double potential = Potential(halite);
+                        halite = 253 * potential / maxPotential; // 0..253
+                        halite += 1; // 1..254
 
                         if (richIsCheap)
                             halite = byte.MaxValue - halite; // 254..1
 
-                        Debug.Assert(halite > 0 && halite < byte.MaxValue); // 1..254
+                        Debug.Assert(halite > 0 && halite < byte.MaxValue, $"{halite}"); // 1..254
+
                         cost = new CostCell((byte)halite);
                     }
 
--- run_seed.bat
+++ run_seed.bat
@@ -1,6 +1,7 @@
 @rem https://github.com/victorkirov/HaliteCSharpStarter
 dotnet build
 @rem -c Debug
-halite.exe --replay-directory replays/ -vvv --width 32 --height 32 "dotnet %cd%\Halite3\bin\Debug\netcoreapp2.0\MyBot.dll" "dotnet %cd%\Halite3\bin\Debug\netcoreapp2.0\MyBot.dll" -s 1544507810
+halite.exe --replay-directory replays/ -vvv --width 64 --height 64 "dotnet %cd%\Halite3\bin\Debug\netcoreapp2.0\MyBot.dll" "dotnet %cd%\Halite3\bin\Debug\netcoreapp2.0\MyBot.dll" -s 1544687260
+@rem -s 1544687260 @rem 115k vs 64k https://halite.io/play/?game_id=3067594&replay_class=0&replay_name=replay-20181213-075002%2B0000-1544687260-64-64-3067594
 @rem -s 1544507810
 @rem -s 1544514780
